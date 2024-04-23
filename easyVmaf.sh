ee4a8ca7c5a732d6bde229fb9653f155f70614e5
Marcel Poelstra Consultancy <marcel@marcelpoelstra.nl>
2024-04-23 21:58:10 +0200

    fix paths in sh script

--- easyVmaf.sh
+++ easyVmaf.sh
@@ -2,6 +2,7 @@
 
 main() {
     local python_cmd
+    local script_dir
 
     # Check for Python3 installation and retrieve the command path
     if ! python_cmd=$(command -v python3 || command -v python); then
@@ -9,8 +10,17 @@ main() {
         return 1
     fi
 
-    # Execute the Python script
-    "$python_cmd" "$(dirname "$(readlink -f "$0")")/easyVmaf.py"
+    # Determine the directory where the Python script is located
+    script_dir=$(dirname "$(readlink -f "$0")")
+
+    # Change to the directory containing the Python script
+    cd "$script_dir" || {
+        printf "Failed to change directory to '%s'.\n" "$script_dir" >&2
+        return 1
+    }
+
+    # Execute the Python script, forwarding all arguments
+    "$python_cmd" "./easyVmaf.py" "$@"
 }
 
 main "$@"
